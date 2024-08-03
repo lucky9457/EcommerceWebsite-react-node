4ed14a64b74a19f441147221a8cec249ccd54903
lucky9457 <luckyreddychintu@gmail.com>
2024-08-03 23:07:45 +0530

    m

--- server.js
+++ server.js
@@ -11,9 +11,6 @@ import productRoutes from "./routes/productRoutes.js";
 
 //configure env
 dotenv.config();
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-
 
 
 //databse config
@@ -26,8 +23,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(express.static(path.join(__dirname, "./clientfrontent/build")))
-
 
 //routes
 app.use("/api/v1/auth", authRoutes);
